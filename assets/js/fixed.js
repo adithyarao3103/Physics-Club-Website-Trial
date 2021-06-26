dbd0cac04cacb9ac8f7730a064973c4c657f7b18
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-26 21:31:51 +0530

    fix

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,10 +1,4 @@
-document.getElementById("close").setAttribute('onclick', 'close()');
-
-function close(){
+function closemenu(){
 	console.log("close");
     document.getElementById("fullscreen").style.visibility = "hidden";
-}
-
-function runclick(){
-	window.alert("clicked!!!!");
 }
