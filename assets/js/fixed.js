0ae5483f4c47abaf9186538690bb7d99b6e3ad79
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-26 21:36:37 +0530

    fix

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,4 +1,9 @@
+function removemenu(){
+    document.getElementById("fullscreen").style.visibility = "hidden";	
+}
+
 function closemenu(){
 	console.log("close");
-    document.getElementById("fullscreen").style.visibility = "hidden";
+    document.getElementById("fullscreen").style.opacity = 0;
+    setTimeout(function(){removemenu();}, 500);
 }
