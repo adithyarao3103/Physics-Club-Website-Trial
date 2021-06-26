78d606a7dd3f782156254f9189b8f09c49289387
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-26 21:21:48 +0530

    fix

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,9 +1,6 @@
-var a = 10;
-
-console.log("js is working");
+document.getElementById("close").setAttribute('onclick', 'close()');
 
 function close(){
 	console.log("close");
-	document.getElementById('fullscreen').style.opacity = 0;
-	document.getElementById('fullscreen').style.transform = 'translateX(200%);';
+    document.getElementById("fullscreen").style.visibility = "hidden";
 }
