e8c50efb9601de47f7dc442041ddebd1aa09b798
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-26 23:41:08 +0530

    fixed menu

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,9 +1,17 @@
 function removemenu(){
-    document.getElementById("fullscreen").style.visibility = "hidden";	
+    document.getElementById("fullscreen").style.display = "none";	
 }
 
 function closemenu(){
 	console.log("close");
     document.getElementById("fullscreen").style.opacity = 0;
     setTimeout(function(){removemenu();}, 500);
+}
+
+function openmenu(){
+	document.getElementById('bar1').style.transform = 'translateX(100px)';
+	document.getElementById('bar2').style.transform = 'translateX(100px)';
+	document.getElementById('bar2').style.transform = 'translateX(100px)';
+	document.getElementById('fullscreen').style.display = "block";
+	document.getElementById('fullscreen').style.opacity= 1;	
 }
