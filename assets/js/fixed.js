371b1b120146828b38605d8b942820b0385f65ad
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-27 00:24:56 +0530

    fix menu

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -5,6 +5,10 @@ function removemenu(){
 function closemenu(){
 	console.log("close");
     document.getElementById("fullscreen").style.opacity = 0;
+	document.getElementById('fullscreen').style.pointerEvents = 'none';
+	document.getElementById('bar1').style.transform = 'translateX(0)';
+	document.getElementById('bar2').style.transform = 'translateX(0)';
+	document.getElementById('bar3').style.transform = 'translateX(0)';
     setTimeout(function(){removemenu();}, 500);
 }
 
@@ -12,7 +16,8 @@ function showmenu(){
 	console.log('menu');
 	document.getElementById('bar1').style.transform = 'translateX(100px)';
 	document.getElementById('bar2').style.transform = 'translateX(100px)';
-	document.getElementById('bar2').style.transform = 'translateX(100px)';
+	document.getElementById('bar3').style.transform = 'translateX(100px)';
 	document.getElementById('fullscreen').style.display = "block";
 	document.getElementById('fullscreen').style.opacity= 1;	
+	document.getElementById('fullscreen').style.pointerEvents = 'auto';
 }
