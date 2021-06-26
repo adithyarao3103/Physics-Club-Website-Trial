1b01a420c1bf85f3b40be5a08f42bcd1ddd3b212
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-26 20:08:35 +0530

    fixes

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,4 +1,5 @@
 function close(){
+	console.log("close");
 	document.getElementById('fullscreen').style.opacity = 0;
 	document.getElementById('fullscreen').style.transform = 'translateX(200%);';
 }
