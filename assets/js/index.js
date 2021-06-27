de738428211bd626ed378d560cda234af9cafefd
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-27 12:06:22 +0530

    main

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,14 @@ image.src = bg;
 
 image.addEventListener('load', (event)=> {
   document.getElementById('topcontent').style.backgroundImage = `url(${bg})`;
-console.log("loaded")});
+});
 
 document.addEventListener('scroll', (event) => {
 	scroly = this.scrollY;
-	if (scroly > 750) {
-		document.getElementById('navbar').background = "white";
+	if (scroly > 75) {
+		document.getElementById('navbar').style.background = "white";
 	}
 	else{
-		document.getElementById('navbar').background = "transparent";
+		document.getElementById('navbar').style.background = "transparent";
 	}
 });
