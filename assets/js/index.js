6987ffa79ccde6363cee633c77fc18e5b719d57f
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-27 16:14:47 +0530

    nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,24 +15,11 @@ document.addEventListener('scroll', (event) => {
 
 	var current_height = document.getElementById("tit").getBoundingClientRect().top;
 
-	if (current_height < req_height-10) {
-		document.getElementById('navbar').style.background = "white";
-		document.getElementById('bar1').style.background = "black";
-		document.getElementById('bar2').style.background = "black";
-		document.getElementById('title').style.color = "black";
-		document.getElementById('bar3').style.background = "black";
+	
 
-	}
-	else{
-		document.getElementById('navbar').style.background = "transparent";
-		document.getElementById('bar1').style.background = "white";
-		document.getElementById('bar2').style.background = "white";
-		document.getElementById('bar3').style.background = "white";
+	if (current_height > req_height-4) {
+		document.getElementById('tit').style.color = "white";
 		document.getElementById('title').style.color = "transparent";
-	}
-
-
-	if (current_height > req_height) {
 		var beg = "scale(";
 		var val = 0.32 + 0.68*(current_height - req_height)/(initial_height - req_height);
 		var end = ")"
@@ -44,6 +31,22 @@ document.addEventListener('scroll', (event) => {
 		document.getElementById("of").style.transform = inner;
 
 	}
+	else{
+		if (current_height < req_height-70) {
+		document.getElementById('navbar').style.background = "white";
+		document.getElementById('bar1').style.background = "black";
+		document.getElementById('bar2').style.background = "black";
+		document.getElementById('bar3').style.background = "black";
+		document.getElementById('title').style.color = "black";
+	}
+	else{
+		document.getElementById('navbar').style.background = "transparent";
+		document.getElementById('bar1').style.background = "white";
+		document.getElementById('bar2').style.background = "white";
+		document.getElementById('bar3').style.background = "white";
+		document.getElementById('title').style.color = "white";
+	}
+}
 });
 
 
