1dfda696cc518e9d1dbd122f9aab7bb12aff7c54
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-27 11:55:15 +0530

    main

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,13 @@ image.src = bg;
 image.addEventListener('load', (event)=> {
   document.getElementById('topcontent').style.backgroundImage = `url(${bg})`;
 console.log("loaded")});
+
+document.addEventListener('scroll', (event) => {
+	scroly = this.scrollY;
+	if (scroly > 750) {
+		document.getElementById('navbar').background = "white";
+	}
+	else{
+		document.getElementById('navbar').background = "transparent";
+	}
+});
