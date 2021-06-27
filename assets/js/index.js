3aab9fd8b6a1febf66bae9001df2565392ee85b6
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-27 12:08:47 +0530

    burger

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,8 +11,15 @@ document.addEventListener('scroll', (event) => {
 	scroly = this.scrollY;
 	if (scroly > 75) {
 		document.getElementById('navbar').style.background = "white";
+		document.getElementById('bar1').style.background = "black";
+		document.getElementById('bar2').style.background = "black";
+		document.getElementById('bar3').style.background = "black";
+
 	}
 	else{
 		document.getElementById('navbar').style.background = "transparent";
+		document.getElementById('bar1').style.background = "white";
+		document.getElementById('bar2').style.background = "white";
+		document.getElementById('bar3').style.background = "white";
 	}
 });
