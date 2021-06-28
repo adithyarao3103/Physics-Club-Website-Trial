2a7a09973d9781799265cfdeae183f1ef30e3f33
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-28 21:29:01 +0530

    Update events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -10,6 +10,7 @@ for(var i=0;i<events.length;i++){
 	entities = [];
 	for (var j = 0; j < childnodes.length; j++) {
 		entities.append(childnodes[j].nodeValue);
+		console.log(entities);
 	}
 
 	inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + entities[3] + '.jpg)"></div></div>';
