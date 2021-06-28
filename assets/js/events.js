72a9f04adc142e1bd08adf439a8bda24cb204c98
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-28 22:28:24 +0530

    json

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,9 +1,26 @@
+ // Create XMLHttpRequest object.
+    var oXHR = new XMLHttpRequest();
+
+    // Initiate request.
+    oXHR.onreadystatechange = reportStatus;
+    oXHR.open("GET", "/assets/json/events.json", true);  // get json file.
+    oXHR.send();
+
+    function reportStatus() {
+        if (oXHR.readyState == 4) {		// Check if request is complete.
+            eventsdata = this.responseText;
+        }
+    }
+
+    
+
 eventsdata = JSON.parse(events);
 
 var inner = '';
 
 for (var i = 0; i < eventsdata.length; i++) {
 inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + eventsdata[i].poster + '.jpg)"></div></div>';
+
 }
 
 
