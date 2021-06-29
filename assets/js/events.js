5cb1a3373f51efb14b01f7dbba0509bb701e5480
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-29 12:13:04 +0530

    events

--- assets/js/events.js
+++ assets/js/events.js
@@ -17,7 +17,7 @@
 function writepage(){
 var inner = '';
 for (var i = 0; i < events.length; i++) {
-inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + events[i].poster + '.jpg)"></div></div>';
+inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + events[i].poster + '.jpg) load="lazy"></div></div>';
 }
 document.getElementById('container').innerHTML = inner;
 }
