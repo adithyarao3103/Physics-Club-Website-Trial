ed4851d58b78a4b82c478002be62393562c3f742
adithyarao3103 <adithyarao3132001@gmail.com>
2021-06-29 14:13:42 +0530

    Update events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -10,15 +10,17 @@
         if (oXHR.readyState == 4) {		// Check if request is complete.
             eventsdata = this.responseText;
             events = JSON.parse(eventsdata).events;
-            writepage()
+            writepage();
+            lazy();
         }
     }
 
 function writepage(){
 var inner = '';
 for (var i = 0; i < events.length; i++) {
-inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + events[i].poster + '.jpg) load="lazy"></div></div>';
+inner += '<div class="item"><div class="bg" style="background-image: url(/assets/posters/' + events[i].poster + '.jpg)"></div></div>';
 }
 document.getElementById('container').innerHTML = inner;
 }
+
 
