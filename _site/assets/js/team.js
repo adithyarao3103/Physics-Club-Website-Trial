1d87ed2409d7c4e0bcf23ac16db10d918a48b7b0
adithyarao3103 <adithyarao3132001@gmail.com>
2021-07-28 21:25:47 +0530

    fixed team page

--- _site/assets/js/team.js
+++ _site/assets/js/team.js
@@ -63,7 +63,8 @@ function writepage(){
 
 */
 
-sliders = document.getElementsByClassName('slider')
+sliders = document.getElementsByClassName('slider');
+alumsliders = document.getElementsByClassName('slider2');
 
 
 for(i=0; i< sliders.length; i++){
@@ -92,3 +93,42 @@ new Splide( sliders[i], {
     }} ).mount();
 
 }
+
+for(i=0; i< alumsliders.length; i++){
+new Splide( alumsliders[i], {
+        speed: 10,
+        lazyLoad: 'nearby',
+        pagination: false,
+   type   : 'slide',
+   trimSpace: true,
+   rewind: true,
+    perPage: 4,
+    start: 0,
+    focus  : 'center',
+    arrow: true,
+    gap: '30px',
+    breakpoints: {
+        1000:{
+            perPage: 3,
+        },
+        760: {
+            perPage: 2,
+        },
+        450: {
+            perPage: 1,
+        },
+    }} ).mount();
+
+}
+
+arrows = document.getElementsByTagName('button')
+function enabled(){
+    for(i=0;i<arrows.length;i++){
+        arrows[i].removeAttribute("disabled");
+    }
+
+    setTimeout(function(){enabled();},1000/60);
+
+}
+
+enabled();
